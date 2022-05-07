2b2ef7cd3cdadc200de4e10bbba8e993a6647b83
RutujaK93 <105098709+RutujaK93@users.noreply.github.com>
2022-05-07 12:40:29 +0530

    initial commit

---
+++ day2/2nd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class demo
+{
+public static void main(String args[])
+{
+
+double r,a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter radius");
+r=sc.nextDouble();
+a=3.14*(r)*(r);
+System.out.println("Area is "+a);
+}
+}
+
+
+
---
+++ day2/day2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class demo
+{
+public static void main(String args[])
+{
+
+float c,f;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter value");
+f=sc.nextFloat();
+c=5/9*(f-32);
+System.out.println("celcius temprature is "+c );
+}
+}
+
+
+
