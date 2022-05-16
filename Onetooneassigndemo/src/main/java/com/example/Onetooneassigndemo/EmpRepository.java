ca1ed3d3f3298dac41056c966643b3a60ba87023
RutujaK93 <105098709+RutujaK93@users.noreply.github.com>
2022-05-16 10:19:03 +0530

    initial commit

---
+++ Onetooneassigndemo/src/main/java/com/example/Onetooneassigndemo/EmpRepository.java
@@ -0,0 +1,10 @@
+package com.example.Onetooneassigndemo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpRepository extends JpaRepository<Emp, Integer> {
+
+	
+	
+
+}
---
+++ Onetooneassigndemo/src/test/java/com/example/Onetooneassigndemo/OnetooneassigndemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Onetooneassigndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetooneassigndemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
